fa75f927a63f1dfe8b0c1785bb9ebb92030408a9
Arnold-Hu <hbc0204@foxmail.com>
2016-05-30 18:00:23 +0800

    把直接划分点集改为附近点集

--- perfect_curve/curve.py
+++ perfect_curve/curve.py
@@ -12,19 +12,19 @@ def line(dots):
     line_x = [x[0] for x in dots]
     line_y = [x[1] for x in dots]
     tmp1 = sum([x[0] * x[1] for x in zip(line_x, line_y)])
-    print "tmp1 %s" % tmp1
+    # print "tmp1 %s" % tmp1
     tmp2 = sum(line_x)*sum(line_y)/length
-    print "tmp2 %s" % tmp2
+    # print "tmp2 %s" % tmp2
     tmp3 = sum([x**2 for x in line_x])
-    print "tmp3 %s" % tmp3
+    # print "tmp3 %s" % tmp3
     tmp4 = sum(line_x)**2/length
-    print "tmp4 %s" % tmp4
-    print "sum y %s" % sum(line_y)
-    print "sum x %s" % sum(line_x)
+    # print "tmp4 %s" % tmp4
+    # print "sum y %s" % sum(line_y)
+    # print "sum x %s" % sum(line_x)
     a = (tmp1 - tmp2)/(tmp3 - tmp4)
-    print "a is %s" % a
+    # print "a is %s" % a
     b = sum(line_y)/length - a*(sum(line_x)/length)
-    print "b is %s" % b
+    # print "b is %s" % b
     return a, b
 
 
@@ -50,21 +50,30 @@ def cut_pieces(dots, pieces):
 
 
 def get_neighbur(dots, x, n):
+    print "x is %s" % x
     length = len(dots)
-    for index, pairs in dots:
+    result = []
+    for index, pairs in enumerate(dots):
         if pairs[0]<= x and dots[index+1][0] > x:
             left = index
             right = index + 1
     for i in range(n):
         if left < 0:
-            left = -999999
-        if right >= length:
-            right = -999999
-        if abs(right - x) < abs(left - x):
-            right -= 1
+            left_value = 99999999
         else:
+            left_value = dots[left][0]
+        if right >= length:
+            right_value = -999999
+        else:
+            right_value = dots[right][0]
+        if abs(x - left_value) < abs(x - right_value):
+            result.append(dots[left])
             left -= 1
-    return left, right
+        else:
+            result.append(dots[right])
+            right += 1
+    print "result is %s"% sorted(result, key = lambda x:x[0])
+    return result, left, right
 
 
 def get_x_dots(dots, num):
@@ -75,7 +84,7 @@ def get_x_dots(dots, num):
 
 def produce_random_point(limit_size=100):
     x = random.random()*100
-    y = math.sin(x/10)*100 + random.random()*30
+    y = math.sin(x/10)*100 + random.random()*100
     # y = 2*x + random.random()*20
     return (x,y)
 
@@ -96,6 +105,8 @@ if __name__ == "__main__":
     n = int(input('>'))
     print u'请输入划分数量：'
     pieces = int(input('>'))
+    print u'请输入依赖的邻居数量：'
+    neibour_num = int(input('>'))
 
     dots= []
     for i in range(n):
@@ -103,18 +114,28 @@ if __name__ == "__main__":
 
     dots.sort(key=lambda x:x[0])
     dots_center = get_x_dots(dots, pieces)
-    dots_sets = cut_pieces(dots, pieces)
+    # dots_sets = cut_pieces(dots, pieces)
+    dots_sets = []
+    for dot in dots_center:
+        neibours, left, right = get_neighbur(dots, dot, neibour_num)
+        dots_sets.append(neibours)
     result = []
-    for index, x in enumerate(reversed(dots_center)):
+    for index, x in enumerate(dots_center):
         a, b = line(dots_sets[index])
         y = a * x + b
         result.append((x,y))
+    print "result is %s"%result
 
     colors = random_color(pieces)
     fig = plt.figure(figsize=(16,12), dpi=72, facecolor="white")
     for i,dot_set in enumerate(dots_sets):
         for dot in dot_set:
-            plt.scatter(dot[0], dot[1], s=10, color=colors[i])
+            pos = dots.index(dot)
+            if pos>=0:
+                dots.pop(pos)
+            plt.scatter(dot[0], dot[1], s=10, color=colors[i], marker='v')
     for i,dot in enumerate(result):
         plt.scatter(dot[0], dot[1], marker='*', s=600, color=colors[i])
+    for i in dots:
+        plt.scatter(i[0], i[1], marker='x')
     plt.show()
